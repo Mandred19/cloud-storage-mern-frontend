59a240b1e826a038969c7e5e5b45e0ca8c8e3a35
Alexander Alexeev <alekseev@trueconf.ru>
2021-06-17 23:18:36 +0300

    base structure

---
+++ src/router/index.tsx
@@ -0,0 +1,12 @@
+import React, {FC, ReactElement} from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+const Router: FC = (): ReactElement => {
+  return (
+    <Switch>
+      {/*<Route path="/" component={}/>*/}
+    </Switch>
+  );
+};
+
+export default Router;
