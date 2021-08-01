dff9046da51620bb85a74ae5a7557d985278d336
Alexander Alexeev <alekseev@trueconf.ru>
2021-08-01 12:03:39 +0300

    add auth componenets

--- src/screens/Authorization/AuthorizationLogin.tsx
+++ src/screens/Authorization/AuthorizationLogin.tsx
@@ -1,16 +1,100 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, FormEvent, ReactElement, useState} from 'react';
+import { Link } from 'react-router-dom';
 
-// import {createStyles, makeStyles, Theme} from '@material-ui/core';
+import {Button, Container, createStyles, makeStyles, TextField, Theme, Typography} from '@material-ui/core';
 
-// const useStyles = makeStyles((theme: Theme) => createStyles({}));
+import {inputChangeEventType, inputChangeHandler} from '../../helpers/inputChangeHandler';
+import {betweenChildrenMixin} from '../../theme/styleMixins';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  container: {
+    margin: 'auto',
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexFlow: 'column nowrap',
+  },
+  body: {
+    marginBottom: theme.spacing(2),
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+  },
+  input: {
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexFlow: 'column nowrap',
+    ...betweenChildrenMixin({marginBottom: theme.spacing()}),
+  },
+}));
 
 const AuthorizationLogin: FC = (): ReactElement => {
-  // const classes = useStyles();
+  const classes = useStyles();
+
+  const [login, setLogin] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const logIn = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.warn(login, password);
+    setLogin('');
+    setPassword('');
+  };
 
   return (
-    <section>
-      AuthorizationLogin
-    </section>
+    <Container component={'section'} maxWidth={'xs'} className={classes.container}>
+      <div className={classes.header}>
+        <Typography variant={'h5'} className={classes.title}>
+          Log in to storage
+        </Typography>
+      </div>
+
+      <form onSubmit={logIn} className={classes.body} autoComplete="off">
+        <TextField
+          onChange={(e: inputChangeEventType) => inputChangeHandler(e, setLogin)}
+          value={login}
+          type={'text'}
+          variant={'outlined'}
+          className={classes.input}
+          autoFocus
+          fullWidth
+          label={'Phone, email, or username'}
+          placeholder={'Phone, email, or username'}/>
+
+        <TextField
+          onChange={(e: inputChangeEventType) => inputChangeHandler(e, setPassword)}
+          value={password}
+          type={'password'}
+          variant={'outlined'}
+          className={classes.input}
+          fullWidth
+          label={'Password'}
+          placeholder={'Password'}/>
+
+        <Button type={'submit'} variant={'contained'} color={'primary'} fullWidth>
+          Log in
+        </Button>
+      </form>
+
+      <div className={classes.footer}>
+        <Typography variant={'body2'}>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          I don't have an account
+        </Typography>
+
+        <Link to={'/sign-up'} className={'router-link'}>
+          <Typography color={'primary'} variant={'button'}>
+            Sign up
+          </Typography>
+        </Link>
+      </div>
+    </Container>
   );
 };
 
--- src/screens/Authorization/AuthorizationRegistration.tsx
+++ src/screens/Authorization/AuthorizationRegistration.tsx
@@ -1,16 +1,100 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, FormEvent, ReactElement, useState} from 'react';
+import {Link} from 'react-router-dom';
 
-// import {createStyles, makeStyles, Theme} from '@material-ui/core';
-//
-// const useStyles = makeStyles((theme: Theme) => createStyles({}));
+import {Button, Container, createStyles, makeStyles, TextField, Theme, Typography} from '@material-ui/core';
+
+import {inputChangeEventType, inputChangeHandler} from '../../helpers/inputChangeHandler';
+import {betweenChildrenMixin} from '../../theme/styleMixins';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  container: {
+    margin: 'auto',
+  },
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexFlow: 'column nowrap',
+  },
+  body: {
+    marginBottom: theme.spacing(2),
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+  },
+  input: {
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexFlow: 'column nowrap',
+    ...betweenChildrenMixin({marginBottom: theme.spacing()}),
+  },
+}));
 
 const AuthorizationRegistration: FC = (): ReactElement => {
-  // const classes = useStyles();
+  const classes = useStyles();
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+
+  const signUp = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.warn(name, email);
+    setName('');
+    setEmail('');
+  };
 
   return (
-    <section>
-      AuthorizationRegistration
-    </section>
+    <Container component={'section'} maxWidth={'xs'} className={classes.container}>
+      <div className={classes.header}>
+        <Typography variant={'h5'} className={classes.title}>
+          Create your account
+        </Typography>
+      </div>
+
+      <form onSubmit={signUp} autoComplete="off" className={classes.body}>
+        <TextField
+          onChange={(e: inputChangeEventType) => inputChangeHandler(e, setName)}
+          value={name}
+          type={'text'}
+          variant={'outlined'}
+          className={classes.input}
+          autoFocus
+          fullWidth
+          label={'Name'}
+          placeholder={'Name'}/>
+
+        <TextField
+          onChange={(e: inputChangeEventType) => inputChangeHandler(e, setEmail)}
+          value={email}
+          type={'email'}
+          variant={'outlined'}
+          className={classes.input}
+          fullWidth
+          label={'Email'}
+          placeholder={'Email'}/>
+
+        <Button type={'submit'} variant={'contained'} color={'primary'} fullWidth>
+          Sign up
+        </Button>
+      </form>
+
+      <div className={classes.footer}>
+        <Typography variant={'body2'}>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          I already have an account
+        </Typography>
+
+        <Link to={'/log-in'} className={'router-link'}>
+          <Typography color={'primary'} variant={'button'}>
+            Log in
+          </Typography>
+        </Link>
+      </div>
+    </Container>
   );
 };
 
